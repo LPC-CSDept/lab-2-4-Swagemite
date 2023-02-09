d7aaf8b531e6bf7575fbc3d66b003870fb5d7d9e
Brent Young <bryoung777@icloud.com>
2023-02-08 18:05:35 -0800

    Attempt 2?

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     ##################################################
     # Comlete your code here
+    str = ['Python', ' Programming']
+    sub1 = str[0]
+    sub2 = str[-1]
+    print(sub2)
+    print(sub1)
+    print(sub1 + sub2)
     ##################################################
     pass
 
